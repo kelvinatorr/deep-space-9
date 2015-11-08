d570f64e17afb9c50106fe1c5b321b5eaeff6c7d
kelvinatorr <kelvin.dealca@gmail.com>
2015-11-08 09:01:26 -0800

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name deepspace9App
+ * @description
+ * # deepspace9App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('deepspace9App', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deepspace9App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deepspace9App
+ */
+angular.module('deepspace9App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
