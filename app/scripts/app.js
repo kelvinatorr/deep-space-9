c6f137f7a8ab8927b75c898fca16afc53beafb43
kelvinatorr <kelvin.dealca@gmail.com>
2015-11-12 20:15:51 -0800

    add admin state, view and controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,12 +14,28 @@
       'ngAnimate','ui.router'
     ]);
 
-  app.config(['$compileProvider','$httpProvider','$stateProvider', '$urlRouterProvider', '$urlMatcherFactoryProvider', AppConfig]);
+  app.config(['$compileProvider','$stateProvider', '$urlRouterProvider', '$urlMatcherFactoryProvider', AppConfig]);
 
-  function AppConfig($compileProvider, $httpProvider, $stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
+  function AppConfig($compileProvider, $stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
       $urlMatcherFactoryProvider.caseInsensitive(true);
       // ignore trailing slashes.
       $urlMatcherFactoryProvider.strictMode(false);
+
+      $stateProvider
+          .state('main', {
+              url: '/main',
+              templateUrl: 'views/main.html',
+              controller: 'MainCtrl',
+              controllerAs: 'ctrl'
+          })
+          .state('admin', {
+              url: '/admin',
+              templateUrl: 'views/admin.html',
+              controller: 'AdminCtrl',
+              controllerAs: 'ctrl'
+          });
+
+      $urlRouterProvider.otherwise('/main');
       // Remove debug info when in production.
       if(window.location.host.split(':')[0] !== 'localhost') {
           $compileProvider.debugInfoEnabled(false);
