1a473c8c6bfdbcdf902363f0be032e937dd68849
kelvinatorr <kelvin.dealca@gmail.com>
2015-11-12 20:04:21 -0800

    yo angular setup and configuration

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,30 @@
-'use strict';
+(function(){
 
-/**
- * @ngdoc overview
- * @name deepspace9App
- * @description
- * # deepspace9App
- *
- * Main module of the application.
- */
-angular
-  .module('deepspace9App', [
-    'ngAnimate'
-  ]);
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name deepspace9App
+   * @description
+   * # deepspace9App
+   *
+   * Main module of the application.
+   */
+  var app = angular.module('deepspace9App', [
+      'ngAnimate','ui.router'
+    ]);
+
+  app.config(['$compileProvider','$httpProvider','$stateProvider', '$urlRouterProvider', '$urlMatcherFactoryProvider', AppConfig]);
+
+  function AppConfig($compileProvider, $httpProvider, $stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
+      $urlMatcherFactoryProvider.caseInsensitive(true);
+      // ignore trailing slashes.
+      $urlMatcherFactoryProvider.strictMode(false);
+      // Remove debug info when in production.
+      if(window.location.host.split(':')[0] !== 'localhost') {
+          $compileProvider.debugInfoEnabled(false);
+      }
+
+  }
+})();
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,25 @@
-'use strict';
+(function() {
 
-/**
- * @ngdoc function
- * @name deepspace9App.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the deepspace9App
- */
-angular.module('deepspace9App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name deepspace9App.controller:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the deepspace9App
+   */
+  angular.module('deepspace9App')
+    .controller('MainCtrl', MainCtrl);
+
+  function MainCtrl($scope) {
+      $scope.awesomeThings = [
+          'HTML5 Boilerplate',
+          'AngularJS',
+          'Karma'
+      ];
+
+
+  }
+})();
+
