17a46b46c5dee314a2c645b76c24e9651a11b141
kelvinatorr <kelvin.dealca@gmail.com>
2016-01-04 20:41:55 -0800

    add material design icon set to mdIconProvider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -82,13 +82,27 @@
           .warnPalette('red');
 
       $mdIconProvider
-          .defaultIconSet("./images/avatars.svg", 128)
-          .icon("menu"       , "./images/menu.svg"        , 24)
           .icon("share"      , "./images/share.svg"       , 24)
-          .icon("google_plus", "./images/google_plus.svg" , 512)
-          .icon("hangouts"   , "./images/hangouts.svg"    , 512)
-          .icon("twitter"    , "./images/twitter.svg"     , 512)
-          .icon("phone"      , "./images/phone.svg"       , 512);
+          .defaultIconSet('images/material-icons/svg-sprite-maps.svg', 128)
+          .icon('menu', 'images/material-icons/menu.svg', 24)
+          //.iconSet("glyphicons", "fonts/glyphicons-halflings-regular.svg", 24);
+          .icon('call', 'images/material-icons/ic_call_24px.svg', 24)
+          .iconSet('action', 'images/material-icons/action-icons.svg', 24)
+          .iconSet('alert', 'images/material-icons/alert-icons.svg', 24)
+          .iconSet('av', 'images/material-icons/av-icons.svg', 24)
+          .iconSet('communication', 'images/material-icons/communication-icons.svg', 24)
+          .iconSet('content', 'images/material-icons/content-icons.svg', 24)
+          .iconSet('device', 'images/material-icons/device-icons.svg', 24)
+          .iconSet('editor', 'images/material-icons/editor-icons.svg', 24)
+          .iconSet('file', 'images/material-icons/file-icons.svg', 24)
+          .iconSet('hardware', 'images/material-icons/hardware-icons.svg', 24)
+          .iconSet('icons', 'images/material-icons/icons-icons.svg', 24)
+          .iconSet('image', 'images/material-icons/image-icons.svg', 24)
+          .iconSet('maps', 'images/material-icons/maps-icons.svg', 24)
+          .iconSet('navigation', 'images/material-icons/navigation-icons.svg', 24)
+          .iconSet('notification', 'images/material-icons/notification-icons.svg', 24)
+          .iconSet('social', 'images/material-icons/social-icons.svg', 24)
+          .iconSet('toggle', 'images/material-icons/toggle-icons.svg', 24);
 
   }
 })();
