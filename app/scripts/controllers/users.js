0323b0a67d46a1754d397016927bed6a013bcbd0
kelvinatorr <kelvin.dealca@gmail.com>
2016-01-04 20:02:12 -0800

    adapt side nav code to admin users

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -13,12 +13,77 @@
     angular.module('deepspace9App')
         .controller('UsersCtrl', UsersCtrl);
 
-    function UsersCtrl($scope) {
+    function UsersCtrl($scope, $timeout, $mdSidenav, $log) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
+
+        document.getElementById('testSubList').style.height = 0 + 'px';
+
+        var subListShowing = false;
+
+        $scope.toggleSublist = function() {
+            console.log('changing!');
+            if(!subListShowing) {
+                var height = 0;
+                for(var i = 0; i < 48 * 2; i++) {
+                    height += i;
+                    console.log(height);
+                    document.getElementById('testSubList').style.height = i + 'px';
+                }
+            } else {
+                document.getElementById('testSubList').style.height = 0 + 'px';
+            }
+            subListShowing = !subListShowing;
+            //$scope.$apply();
+        };
+
+        //$scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleLeft = buildToggler('left');
+        $scope.isOpenLeft = function(){
+            return $mdSidenav('left').isOpen();
+        };
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        //function debounce(func, wait, context) {
+        //    var timer;
+        //    return function debounced() {
+        //        var context = $scope,
+        //            args = Array.prototype.slice.call(arguments);
+        //        $timeout.cancel(timer);
+        //        timer = $timeout(function() {
+        //            timer = undefined;
+        //            func.apply(context, args);
+        //        }, wait || 10);
+        //    };
+        //}
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        //function buildDelayedToggler(navID) {
+        //    return debounce(function() {
+        //        $mdSidenav(navID)
+        //            .toggle()
+        //            .then(function () {
+        //                $log.debug("toggle " + navID + " is done");
+        //            });
+        //    }, 200);
+        //}
+        function buildToggler(navID) {
+            return function() {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            };
+        }
     }
 
 })();
