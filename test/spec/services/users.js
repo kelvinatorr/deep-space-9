21b64a83e21d5fbb54d2e0e488a4c8958a7b8868
kelvinatorr <kelvin.dealca@gmail.com>
2016-02-01 11:29:42 -0800

    add unit test for deleteUser on Users factory

--- test/spec/services/users.js
+++ test/spec/services/users.js
@@ -7,13 +7,40 @@ describe('Service: Users', function () {
 
     // instantiate service
     var Users;
+
+    var firebaseMethods = {
+        update: function() {}
+    };
+
+    var mockRef = {
+        child: function() {
+            return firebaseMethods;
+        }
+    };
+
+    beforeEach(module(function($provide) {
+        $provide.value('FirebaseRef', {
+            ref: mockRef
+        });
+    }));
     beforeEach(inject(function (_Users_) {
+        // add jasmine spies
+        spyOn(firebaseMethods, 'update');
         Users = _Users_;
     }));
 
     it('should do something', function () {
         expect(!!Users).toBe(true);
-        console.log(Users);
+        //console.log(Users);
+    });
+
+    it('should call firebase update on deleteUser', function () {
+        var deleteList = [
+            {$id: 1},
+            {$id: 2}
+        ];
+        Users.deleteUser(deleteList);
+        expect(firebaseMethods.update).toHaveBeenCalled();
     });
 
 });
