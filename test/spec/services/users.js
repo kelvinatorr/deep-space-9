a7e222520d3365e8db18eff6d2c7299da84758f2
kelvinatorr <kelvin.dealca@gmail.com>
2016-02-05 10:06:09 -0800

    organize tests into describes and add more tests for getUsers

--- test/spec/services/users.js
+++ test/spec/services/users.js
@@ -80,36 +80,77 @@ describe('Service: Users', function () {
         //console.log(Users);
     });
 
-    it('should call firebase update on deleteUser', function () {
-        var deleteList = [
-            {$id: 1},
-            {$id: 2}
-        ];
-        Users.deleteUser(deleteList);
-        expect(firebaseMethods.update).toHaveBeenCalled();
+    describe('deleteUser', function() {
+        it('should call firebase update on deleteUser', function () {
+            var deleteList = [
+                {$id: 1},
+                {$id: 2}
+            ];
+            Users.deleteUser(deleteList);
+            expect(firebaseMethods.update).toHaveBeenCalled();
+        });
+
+        it('should return a promise', function() {
+            var deleteList = [
+                {$id: 1},
+                {$id: 2}
+            ];
+            var result = Users.deleteUser(deleteList);
+            expect('then' in result).toBe(true);
+        });
+
     });
 
-    it('should call firebase createUser on ref on createUser', function () {
-        var newUser = {
-            lastName: 'Boom',
-            temporaryPassword: 'headshot',
-            email: 'boom@example.com'
-        };
-        Users.createUser(newUser);
-        expect(mockRef.createUser).toHaveBeenCalled();
+
+    describe('createUser', function(){
+        it('should call firebase createUser', function () {
+            var newUser = {
+                lastName: 'Boom',
+                temporaryPassword: 'headshot',
+                email: 'boom@example.com'
+            };
+            Users.createUser(newUser);
+            expect(mockRef.createUser).toHaveBeenCalled();
+        });
     });
 
-    it('should set total users property to 1', function() {
+
+
+    describe('getUsers', function() {
+
         var query = {
             order: 'lastName',
             limit: 5,
             page: 1
         };
 
-        Users.getUsers(query);
-        deferred.resolve(usersResult);
-        rootScope.$apply();
-        expect(Users.totalUsers).toBe(usersResult.length);
-    })
+
+        it('should set total users property to the length of usersResult', function() {
+            Users.getUsers(query);
+            deferred.resolve(usersResult);
+            rootScope.$apply();
+            expect(Users.totalUsers).toBe(usersResult.length);
+        });
+
+        it('should set table data to be usersResult', function() {
+            Users.getUsers(query);
+            deferred.resolve(usersResult);
+            rootScope.$apply();
+            expect(Users.tableData).toEqual(usersResult);
+        });
+
+        it('should resolve to the factory object', function() {
+            var result;
+            Users.getUsers(query).then(function(obj) {
+                result = obj;
+            });
+            deferred.resolve(usersResult);
+            rootScope.$apply();
+            expect(result).toEqual(Users);
+        });
+
+    });
+
+
 
 });
