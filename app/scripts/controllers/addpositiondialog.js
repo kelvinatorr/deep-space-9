7ada930c626754de6078278e2ac18241d38351a4
kelvinatorr <kelvin.dealca@gmail.com>
2016-03-20 08:30:43 -0700

    Add scaffolding for add-position-dialog
    Add html file, add controller, fix controller's tests

---
+++ app/scripts/controllers/addpositiondialog.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc function
+     * @name deepspace9App.controller:AddpositiondialogCtrl
+     * @description
+     * # AddpositiondialogCtrl
+     * Controller of the deepspace9App
+     */
+    angular.module('deepspace9App')
+        .controller('AddPositionDialogCtrl', AddPositionDialogCtrl);
+
+    function AddPositionDialogCtrl() {
+
+    }
+
+})();
--- app/scripts/controllers/positions.js
+++ app/scripts/controllers/positions.js
@@ -12,12 +12,39 @@
     angular.module('deepspace9App')
         .controller('PositionsCtrl', PositionsCtrl);
 
-    function PositionsCtrl(positions, client) {
+    function PositionsCtrl(positions, client, $mdDialog) {
         var vm = this;
 
         vm.positions = positions;
 
         vm.client = client;
+
+        vm.addPosition = addPosition;
+
+        function addPosition(ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            var dialogPromise = $mdDialog.show(
+                {
+                    controller: 'AddPositionDialogCtrl',
+                    controllerAs: 'vm',
+                    locals: {
+                        action: 'Add'
+                    },
+                    bindToController: true,
+                    templateUrl: 'views/add-position-dialog.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose:true
+                }
+            );
+
+            dialogPromise.then(function(newPosition) {
+                positions.createPosition(newPosition).catch(function(error) {
+                    alert(error);
+                });
+            });
+        }
 
     }
 })();
