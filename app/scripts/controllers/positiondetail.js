72666683fb345f717a2c66511aadc613f88644de
kelvinatorr <kelvin.dealca@gmail.com>
2016-03-23 09:50:50 -0700

    Add function stubs for features

--- app/scripts/controllers/positiondetail.js
+++ app/scripts/controllers/positiondetail.js
@@ -15,7 +15,39 @@
         var vm = this;
 
         vm.positionDetail = positionDetail;
-        console.log(vm.positionDetail);
+
+        vm.edit = edit;
+
+        vm.addCandidate = addCandidate;
+
+        vm.addNote = addNote;
+
+        vm.addFile = addFile;
+
+        vm.editNote = editNote;
+
+        /**
+         * Edit the name, description, or priority of the position
+         */
+        function edit() {
+
+        }
+
+        function addCandidate() {
+
+        }
+
+        function addNote() {
+
+        }
+
+        function addFile() {
+
+        }
+
+        function editNote(note) {
+
+        }
     }
 
 })();
