5d714ea5c91c0d48ed375354fc06a1a475292fe1
kelvinatorr <kelvin.dealca@gmail.com>
2016-03-24 09:16:04 -0700

    Add tel format filter

---
+++ app/scripts/filters/tel.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc filter
+     * @name deepspace9App.filter:tel
+     * @function
+     * @description
+     * # tel
+     * Filter in the deepspace9App.
+     */
+    angular.module('deepspace9App')
+        .filter('tel', tel);
+
+    function tel() {
+        return function (tel) {
+            if (!tel) { return ''; }
+
+            var value = tel.toString().trim().replace(/^\+/, '');
+
+            if (value.match(/[^0-9]/)) {
+                return tel;
+            }
+
+            var country, city, number;
+
+            switch (value.length) {
+                case 10: // +1PPP####### -> C (PPP) ###-####
+                    country = 1;
+                    city = value.slice(0, 3);
+                    number = value.slice(3);
+                    break;
+
+                case 11: // +CPPP####### -> CCC (PP) ###-####
+                    country = value[0];
+                    city = value.slice(1, 4);
+                    number = value.slice(4);
+                    break;
+
+                case 12: // +CCCPP####### -> CCC (PP) ###-####
+                    country = value.slice(0, 3);
+                    city = value.slice(3, 5);
+                    number = value.slice(5);
+                    break;
+
+                default:
+                    return tel;
+            }
+
+            if (country == 1) {
+                country = '';
+            }
+
+            number = number.slice(0, 3) + '-' + number.slice(3);
+
+            return (country + ' (' + city + ') ' + number).trim();
+        };
+    }
+
+})();
